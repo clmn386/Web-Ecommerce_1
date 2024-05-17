ae7f31aacafccbae9cc84e41359452a39b69c293
clmn386 <clmn386@gmail.com>
2024-05-17 02:03:07 +0200

    Creando el router y primera estructura del admin

---
+++ pages/ordenes.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout";
+
+export default function Ordenes() {
+    return(
+        <Layout>
+            Pagina Principal
+        </Layout>
+    )
+}
---
+++ pages/producto.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout"
+
+export default function Producto() {
+    return (
+        <Layout>
+            Pagina Productos 
+        </Layout>
+    )
+}
